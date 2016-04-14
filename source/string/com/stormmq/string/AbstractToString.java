f50e0c547f9c1f221fe279f3fd9e9d3b6f565509
Raphael Cohn <raphael.cohn@stormmq.com>
2016-04-14 18:28:54 +0100

    New to string logic

--- source/string/com/stormmq/string/AbstractToString.java
+++ source/string/com/stormmq/string/AbstractToString.java
@@ -24,8 +24,71 @@ package com.stormmq.string;
 
 import org.jetbrains.annotations.*;
 
+import java.util.*;
+import java.util.function.Function;
+
+import static com.stormmq.string.StringConstants.*;
+import static java.util.Arrays.deepToString;
+
 public abstract class AbstractToString
 {
+	@NotNull private static final Map<Class<?>, Function<Object, String>> PrimitiveArrayFormatters = primitiveArrayFormatters();
+
+	@NotNull
+	private static Map<Class<?>, Function<Object, String>> primitiveArrayFormatters()
+	{
+		final Map<Class<?>, Function<Object, String>> primitiveArrayFormatters = new HashMap<>(8);
+		primitiveArrayFormatters.put(boolean.class, argument -> Arrays.toString((boolean[]) argument));
+		primitiveArrayFormatters.put(byte.class, argument -> Arrays.toString((byte[]) argument));
+		primitiveArrayFormatters.put(char.class, argument -> Arrays.toString((char[]) argument));
+		primitiveArrayFormatters.put(short.class, argument -> Arrays.toString((short[]) argument));
+		primitiveArrayFormatters.put(int.class, argument -> Arrays.toString((int[]) argument));
+		primitiveArrayFormatters.put(long.class, argument -> Arrays.toString((long[]) argument));
+		primitiveArrayFormatters.put(float.class, argument -> Arrays.toString((float[]) argument));
+		primitiveArrayFormatters.put(double.class, argument -> Arrays.toString((double[]) argument));
+		return primitiveArrayFormatters;
+	}
+
+	@SuppressWarnings("WeakerAccess")
+	@NotNull
+	public static String toStringAsTuple(@NotNull final Object instance, @NotNull final Object... arguments)
+	{
+		final StringBuilder stringBuilder = new StringBuilder(1024);
+		stringBuilder.append(instance.getClass().getSimpleName());
+		stringBuilder.append('(');
+		final int length = arguments.length;
+		for (int index = 0; index < length; index++)
+		{
+			if (index != 1)
+			{
+				stringBuilder.append(", ");
+			}
+
+			@Nullable final Object argument = arguments[index];
+			final String value;
+			if (argument == null)
+			{
+				value = _null;
+			}
+			else
+			{
+				final Class<?> argumentClass = argument.getClass();
+				if (argumentClass.isArray())
+				{
+					final Class<?> componentType = argumentClass.getComponentType();
+					value = PrimitiveArrayFormatters.getOrDefault(componentType, argumentX -> deepToString((Object[]) argumentX)).apply(argument);
+				}
+				else
+				{
+					value = argument.toString();
+				}
+			}
+			stringBuilder.append(value);
+		}
+		stringBuilder.append(')');
+		return stringBuilder.toString();
+	}
+
 	protected AbstractToString()
 	{
 	}
@@ -45,5 +108,18 @@ public abstract class AbstractToString
 	@Override
 	@NotNull
 	@NonNls
-	public abstract String toString();
+	public final String toString()
+	{
+		return toStringAsTuple(this, fields());
+	}
+
+	@NotNull
+	protected abstract Object[] fields();
+
+	@SuppressWarnings("OverloadedVarargsMethod")
+	@NotNull
+	protected static Object[] fields(@NotNull final Object... fields)
+	{
+		return fields;
+	}
 }
